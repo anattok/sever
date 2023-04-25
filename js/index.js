b25b60897d1cfa2de96347b580de946c3f807ce4
Anatoliy Tokarev <tokarev182@yandex.ru>
2023-04-25 23:43:42 +0300

    add bg catalog card

--- js/index.js
+++ js/index.js
@@ -2,19 +2,30 @@
 const menuButton = document.querySelector(".menu__btn-catalog");
 const droppedMenu = document.querySelector(".catalog-top");
 //меню выезжает по ховеру
-menuButton.addEventListener("mouseover", (e) => {
-  console.log(e.target);
+menuButton.addEventListener("click", (e) => {
   if (
-    e.target === menuButton &&
+    e.currentTarget === menuButton &&
     !droppedMenu.classList.contains("catalog-top--active")
   ) {
     droppedMenu.classList.add("catalog-top--active");
   }
 });
-//меню заезжает при уходе курсора с droppedMenu
-droppedMenu.addEventListener("mouseout", (e) => {
-  console.log(e.target);
-  if (e.target === droppedMenu ) {
+
+// меню заезжает при уходе курсора с droppedMenu
+// droppedMenu.addEventListener("mouseout", (e) => {
+//   e.stopPropagation();
+//   if (
+//     e.currentTarget === droppedMenu &&
+//     droppedMenu.classList.contains("catalog-top--active")
+//   ) {
+//     droppedMenu.classList.remove("catalog-top--active");
+//   }
+//   console.log(e.currentTarget)
+// });
+
+if (droppedMenu.classList.contains("catalog-top--active")) {
+  window.addEventListener("click", (e) => {
+    console.log(e.currentTarget);
     droppedMenu.classList.remove("catalog-top--active");
-  }
-});
+  });
+}
